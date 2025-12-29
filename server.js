26a2c7c210c592bec658cf6f83c0b23f507de21a
AkaneSakuramori <utkarshffuff@gmail.com>
2025-12-29 23:55:14 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,30 @@
+import express from "express";
+import fs from "fs";
+import path from "path";
+import { generatePoster } from "./generate.js";
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.use("/assets", express.static("assets"));
+app.use("/output", express.static("output"));
+
+app.post("/generate", async (req, res) => {
+  const data = req.body;
+
+  fs.writeFileSync(
+    "data/poster.json",
+    JSON.stringify(data, null, 2)
+  );
+
+  await generatePoster();
+
+  res.redirect("/output/poster.png");
+});
+
+app.listen(PORT, () => {
+  console.log(`Poster Generator running on http://localhost:${PORT}`);
+});
