0dd6ea21522b8e2ef684953c7a3348386a1358b7
AkaneSakuramori <utkarshffuff@gmail.com>
2025-12-29 23:59:55 +0530

    Update server.js

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.use("/assets", express.static("assets"));
 app.use("/output", express.static("output"));
+app.use("/fonts", express.static("fonts"));
 
 app.post("/generate", async (req, res) => {
   const data = req.body;
